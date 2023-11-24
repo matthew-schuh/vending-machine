6a3b9d1f99b65626410627ef018ff75d6b417654
Matt Schuh <schuhm92@gmail.com>
2023-11-24 14:16:45 -0600

    adding basic UI

--- Razor/wwwroot/js/site.js
+++ Razor/wwwroot/js/site.js
@@ -1,4 +1,10 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Holds container for vending machine products.
+let $products;
+
+// jQuery function when DOM is ready.
+$(document).ready(async () => {
+    $('#loader').remove();
+});
